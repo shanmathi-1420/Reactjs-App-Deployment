0215d6b4ecabbfa292a80eec39d51bdd6621c4a2
shanmathi-1420 <143080319+shanmathi-1420@users.noreply.github.com>
2023-11-10 16:11:06 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-# Check the Git branch
+# Git branch
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
-    # Build your project
+    # Building project
     ./build.sh
 
-    # Log in to Docker Hub 
+    # Logging into Docker Hub 
     docker login -u shanmathi20 -p dckr_pat_sxr5vV70lVXyBCrE0E_hx-aBm1c
 
   
-    # Tag the image
+    # Tagging the image
     docker tag reactjsimg shanmathi0/dev_repo
 
     # Push the image to the Dev Docker Hub repository
     docker push shanmathi20/dev_repo
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
-    # Build your project
+    # Building project
     ./build.sh
 
-    # Log in to Docker Hub
+    # Logging into Docker Hub
     docker login -u shanmathi20 -p dckr_pat_sxr5vV70lVXyBCrE0E_hx-aBm1c
 
    
-    # Tag the image
+    # Tagging the image
     docker tag reactjsimg shanmathi20/prod_repo 
 
     # Push the image to the Prod Docker Hub repository
